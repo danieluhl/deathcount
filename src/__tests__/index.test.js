317ab4c462fef2c449e014aa3d20292b3ac259b5
Dan Uhl <duhl@wayfair.com>
2022-09-09 06:34:04 -0400

    new failing test

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -3,10 +3,15 @@ const {getFileList} = require('../index');
 
 const MOCK_DIR = __dirname + '/mock';
 
-test('counts instances of word in directory', () => {
+test('gets all files in a directory', () => {
   const files = getFileList(MOCK_DIR);
   const expectFiles = ['testfile1.js', 'testfile2.js', 'subdir/sub-testfile.js'];
   expect(files).toEqual(expectFiles);
 });
 
-
+test('finds files with regex pattern', () => {
+  const REGEX = "yaml";
+  const patternMatchedFiles = getFilesByPattern(REGEX);
+  const expectedMatchedFiles = ['testfile2.js'];
+  expect(patternMatchedFiles).toEqual(expectedMatchedFiles);
+});
